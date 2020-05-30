57e67c9da6d7baee5aac5fde72dbc8556be85f6a
FlailingFog <65811931+FlailingFog@users.noreply.github.com>
2020-05-29 21:20:06 -0400

    Changed 'ACTIVE' value for KK shapekeys to 90% to prevent clipping
    Added more manual corrections.
    Also, it looks like pmx exporter does not export the lower eyelash shapekeys...

--- 2 Generate KK Shapekeys.py
+++ 2 Generate KK Shapekeys.py
@@ -1,4 +1,4 @@
-##########################################
+############################################
 # GENERATE KK SHAPEKEY SCRIPT
 ##########################################
 #Translate shapekeys to english
@@ -50,16 +50,20 @@ def tidyUp(keyName):
     print(keyName)
     return keyName 
 
+
 for shapekey in bpy.data.shape_keys:
     for keyblock in shapekey.key_blocks:
         #print(keyblock.name)
         keyblock.name = tidyUp(keyblock.name)
+
 
 ######################################################## and then...
 #Combine the shapekeys with this code:
 def whatCat(keyName):
     #EyeWhites are unused because the shape keys for left and right eyes both affect the left eye for some reason
     #Eyelashes1 is used because I couldn't see a difference between the other one and they overlap if both are used
+    #EyelashPos is unused because Eyelashes work better and it overlaps with Eyelashes
+    #eye_nal is unused because I have no idea what it does
     
     eyes = [keyName.find("Eyes"), keyName.find("NoseT"), keyName.find("Eyelashes1")]
     if not all(v == -1 for v in eyes):
@@ -76,10 +80,14 @@ def whatCat(keyName):
 used = []
 inUse = []
 
-#These shapekeys are dependant on other shapekeys
+#These shapekeys have a common dependancy on other shapekeys
 correctionList = ['Lips_u_s_op', 'Lips_u_l_op', 'Lips_e_l_op', 'Lips_o_s_op', 'Lips_o_l_op', 'Lips_neko_op', 'Lips_san_op']
+resetList = ['Fangs_def_op', 'Teeth_def_op', 'Tongue_def_op', 'Teeth_bisyou_op1', 'Tongue_egao_op', 'Teeth_i_l_cl', 'Fangs_i_l_cl', 'Teeth_i_s_cl', 'Fangs_i_s_cl']
 
 counter = len(bpy.data.shape_keys[0].key_blocks)
+
+ACTIVE = .9
+
 #print('-------------------')
 for shapekey in bpy.data.shape_keys:
     for keyblock in shapekey.key_blocks:
@@ -104,27 +112,35 @@ for shapekey in bpy.data.shape_keys:
                     if whatCat(keyblock.name) == whatCat(keyblockCheck.name):
                         #I haven't already used this key right?
                         if (keyblockCheck.name not in used):
-                            keyblockCheck.value = 1
+                            keyblockCheck.value = ACTIVE
                             inUse.append(keyblockCheck.name)
                             #print('Using ' + keyblockCheck.name)
                             
             #Manual corrections
             if (keyblock.name in correctionList):
-                shapekey.key_blocks['Fangs_def_op'].value = 1
-                shapekey.key_blocks['Teeth_def_op'].value = 1
-                shapekey.key_blocks['Tongue_def_op'].value = 1
+                shapekey.key_blocks['Fangs_def_op'].value = ACTIVE
+                shapekey.key_blocks['Teeth_def_op'].value = ACTIVE
+                shapekey.key_blocks['Tongue_def_op'].value = ACTIVE
             
             if (keyblock.name == 'Lips_e_s_op'):
-                shapekey.key_blocks['Fangs_def_op'].value = 1
+                shapekey.key_blocks['Fangs_def_op'].value = ACTIVE
             
             if (keyblock.name == 'Lips_bisyou_op'):
                 shapekey.key_blocks['Teeth_bisyou_op1'].value = 0
             
             if (keyblock.name == 'Lips_tabe_op'):
-                shapekey.key_blocks['Fangs_def_op'].value = 1
-                shapekey.key_blocks['Teeth_def_op'].value = 1
-                shapekey.key_blocks['Tongue_egao_op'].value = 1
-            
+                shapekey.key_blocks['Fangs_def_op'].value = ACTIVE
+                shapekey.key_blocks['Teeth_def_op'].value = ACTIVE
+                shapekey.key_blocks['Tongue_egao_op'].value = ACTIVE
+                
+            if (keyblock.name == 'Lips_i_l_op'):
+                shapekey.key_blocks['Teeth_i_l_cl'].value = ACTIVE
+                shapekey.key_blocks['Fangs_i_l_cl'].value = ACTIVE
+                
+            if (keyblock.name == 'Lips_i_s_op'):
+                shapekey.key_blocks['Teeth_i_s_cl'].value = ACTIVE
+                shapekey.key_blocks['Fangs_i_s_cl'].value = ACTIVE
+                
             
             if (keyblock.name not in used):
                 #print('Creating combined shapekey')
@@ -133,12 +149,16 @@ for shapekey in bpy.data.shape_keys:
                 
             for thing in inUse:
                 used.append(thing)
+                
             #reset used values
             for thing in used:
+                shapekey.key_blocks[thing].value=0
+                
+            #reset values for shapekeys that were used to correct another shapekey
+            for thing in resetList:
                 shapekey.key_blocks[thing].value=0
                 #print('')
 
             
             inUse =[]            
             print('end')
-
