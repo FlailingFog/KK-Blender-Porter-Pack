df8a9d940741eddd4b4cbf9cf75dd91329a8b879
FlailingFog <65811931+FlailingFog@users.noreply.github.com>
2021-01-07 22:11:39 -0500

    Update Link Eyebrow Shapekeys to Body Object.py
    If the name of the eyebrows object appeared at the start of any of the shapekeys, the script would fail

--- Scripts/Link Eyebrow Shapekeys to Body Object.py
+++ Scripts/Link Eyebrow Shapekeys to Body Object.py
@@ -1,24 +1,10 @@
-##########################################
-#LINK EYEBROW SHAPEKEYS TO BODY SCRIPT
-##########################################
+'''
+LINK EYEBROW SHAPEKEYS TO BODY SCRIPT
+- Seperate the eyebrow mesh into its own object
+- Select the eyebrows object, then shift click the body object and run the script to link all the body shapekeys to the eyebrow shapekeys
 
-#   Stole a shapekey script from stackoverflow :^)
-#   https://blender.stackexchange.com/questions/86757/python-how-to-connect-shapekeys-via-drivers
-#
-# Add driver from 'selected' object to target 'active' object.
-# Target values are shape_key values
-# 
-# ShapeKeys must be named after Object for the script to work:
-#
-#      selected Object     = Cube
-#      selected Shape Keys = Cube_abcd
-#
-#      active Object       = Sphere
-#      active Shape Keys   = Sphere_abcd
-#      
-# 1. Select all object candidates to receive a driver. (Select the master shapekey-holder last)
-# 2. Select 'active' object as driver target. 
-# 3. Run script.
+Script 90% stolen from https://blender.stackexchange.com/questions/86757/python-how-to-connect-shapekeys-via-drivers
+'''
 
 import bpy
 
@@ -30,7 +16,7 @@ shapekey_list_string = str(active_obj.data.shape_keys.key_blocks.keys()).lower()
 
 for obj in selected_obj:
     for key in obj.data.shape_keys.key_blocks:
-        if key.name.lower().lstrip(obj.name.lower()) in shapekey_list_string:
+        if key.name.lower() in shapekey_list_string:
             if not key.name == "Basis":
                 skey_driver = key.driver_add('value')
                 skey_driver.driver.type = 'AVERAGE'
@@ -43,7 +29,7 @@ for obj in selected_obj:
                 newVar.type = 'SINGLE_PROP'
                 newVar.targets[0].id_type = 'KEY'
                 newVar.targets[0].id = active_obj.data.shape_keys
-                newVar.targets[0].data_path = 'key_blocks["' + key.name.lstrip(obj.name)+ '"].value' 
+                newVar.targets[0].data_path = 'key_blocks["' + key.name+ '"].value' 
                 skey_driver = key.driver_add('mute')
                 skey_driver.driver.type = 'AVERAGE'
                 #skey_driver.driver.show_debug_info = True
@@ -55,4 +41,4 @@ for obj in selected_obj:
                 newVar.type = 'SINGLE_PROP'
                 newVar.targets[0].id_type = 'KEY'
                 newVar.targets[0].id = active_obj.data.shape_keys
-                newVar.targets[0].data_path = 'key_blocks["' + active_obj.name + key.name.lstrip(obj.name.lower())+ '"].mute'
+                newVar.targets[0].data_path = 'key_blocks["' + key.name+ '"].mute'
