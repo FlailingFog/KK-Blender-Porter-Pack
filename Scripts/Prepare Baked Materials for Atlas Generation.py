4a431410d91f148dc10b3ed53df72ccaee33084f
FlailingFog <65811931+FlailingFog@users.noreply.github.com>
2021-01-09 16:20:02 -0500

    Fix images with transparency, fix empty file list check

--- Scripts/Prepare Baked Materials for Atlas Generation.py
+++ Scripts/Prepare Baked Materials for Atlas Generation.py
@@ -4,7 +4,7 @@ PREPARE MATERIALS FOR ATLAS GENERATION
 - This makes the material templates compatible with the Material Combiner feature in CATS.
 
 Usage:
-- Select an object that has had its materials baked in the 3D viewport
+- Select an object that has had its materials baked
 - Select the folder that holds the textures in the Output Properties tab
 - Run script
 
@@ -30,10 +30,10 @@ for matslot in object.material_slots:
     #Then get the image for this material by matching the material name and the lightstate
     #If there's multiple resolutions, choose the first file in the file list
     currentImage = [file.name for file in files if (material.name in file.name and 'light' in file.name)]
-    imageName = currentImage[0]
-    imagePath = folderpath + imageName
-    
-    if currentImage:
+    try:
+        imageName = currentImage[0]
+        imagePath = folderpath + imageName
+        
         #If no KK export image node exists, create one.
         #If a KK export image node already exists, check the type and toggle the state
         #(ex. light is already loaded in, switch the textures to dark)
@@ -65,9 +65,17 @@ for matslot in object.material_slots:
             #get output node
             outputNode = nodes['Material Output']
             
+            #make mix node for image transparency
+            transpMix = nodes.new('ShaderNodeMixShader')
+            transpMix.location = outputNode.location[0], outputNode.location[1] - 300
+            
+            #make transparency node
+            transpNode = nodes.new('ShaderNodeBsdfTransparent')
+            transpNode.location = transpMix.location[0] - 300, transpMix.location[1]
+            
             #make emissive node
             emissiveNode = nodes.new('ShaderNodeEmission')
-            emissiveNode.location = outputNode.location[0], outputNode.location[1] - 300
+            emissiveNode.location = transpMix.location[0], transpMix.location[1] - 300
             
             #make image node
             imageNode = nodes.new('ShaderNodeTexImage')
@@ -79,20 +87,26 @@ for matslot in object.material_slots:
             bpy.data.images[imageName].pack()
             imageNode.image = bpy.data.images[imageName]
             
-            #link the image node to the emissive node, and send it through a mix shader
+            #link the image node to the emissive node, and send it through the transparency mix shader
             links.new(imageNode.outputs[0], emissiveNode.inputs[0])
-            mixShader = nodes.new('ShaderNodeMixShader')
-            mixShader.location = outputNode.location
+            links.new(imageNode.outputs[1], transpMix.inputs[0])
+            
+            links.new(transpNode.outputs[0], transpMix.inputs[1])
+            links.new(emissiveNode.outputs[0], transpMix.inputs[2])
+
+            #Then send that through the main mix shader
+            mainMix = nodes.new('ShaderNodeMixShader')
+            mainMix.location = outputNode.location
             outputNode.location = outputNode.location[0] + 300, outputNode.location[1]
-            links.new(emissiveNode.outputs[0], mixShader.inputs[2])
+            links.new(transpMix.outputs[0], mainMix.inputs[2])
             
             #make the node currently plugged into the output node go through the mix shader
-            links.new(outputNode.inputs[0].links[0].from_node.outputs[0], mixShader.inputs[1])
-            links.new(mixShader.outputs[0], outputNode.inputs[0])
+            links.new(outputNode.inputs[0].links[0].from_node.outputs[0], mainMix.inputs[1])
+            links.new(mainMix.outputs[0], outputNode.inputs[0])
             
-            #set the mix shader's factor to 1 so the image is showing instead of the material
-            mixShader.inputs[0].default_value=1
-    else:
-        print('no image found for this material: ' + material)
+            #set the mix shader's factor to 1 so the baked image is showing instead of the material
+            mainMix.inputs[0].default_value=1
+    except:
+        print('no image found for this material: ' + material.name)
         
 print({'FINISHED'})
