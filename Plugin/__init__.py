1555f28cb4b93a0a4a79dffe37280a4c043d2756
FlailingFog <65811931+FlailingFog@users.noreply.github.com>
2021-03-03 20:22:03 -0500

    Create __init__.py

---
+++ Plugin/__init__.py
@@ -0,0 +1,31 @@
+#words words words
+
+bl_info = {
+    "name" : "KKBlenderPack",
+    "author" : "a blendlet",
+    "description" : "Scripts for automating the cleanup process for a Koikatsu export. See https://github.com/FlailingFog/KK-Blender-Shader-Pack/ for info",
+    "blender" : (2, 91, 2),
+    "location" : "View3D",
+    "warning" : "",
+    "category" : "Generic"
+}
+
+import bpy
+
+from . beforeCATS import before_CATS
+from . shapekeys import shape_keys
+from . separateBody import separate_Body
+from . importTemplates import import_Templates
+from . importTextures import import_Textures
+from . cleanArmature import clean_Armature
+from . bonedrivers import bone_drivers
+
+
+from . KKPanel    import KK_Panel
+
+classes = (before_CATS, shape_keys, separate_Body, import_Templates, import_Textures, clean_Armature, bone_drivers, KK_Panel)
+
+register, unregister = bpy.utils.register_classes_factory(classes)
+
+if __name__ == "__main__":
+    register()
