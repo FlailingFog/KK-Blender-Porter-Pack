26466dcca92d95d5cd9f91806c99618a246f2bf9
FlailingFog <65811931+FlailingFog@users.noreply.github.com>
2021-03-14 09:42:46 -0400

    add fancy checkboxes and number inputs

--- Plugin/KKPanel.py
+++ Plugin/KKPanel.py
@@ -1,5 +1,19 @@
 import bpy
+from bpy.types import Panel, PropertyGroup, Scene, WindowManager
+from bpy.props import (
+    IntProperty,
+    EnumProperty,
+    BoolProperty,
+    StringProperty,
+    PointerProperty,
+)
 
+class PlaceholderProperties(PropertyGroup):
+    inc_dec_int: IntProperty(
+        name="Incr-Decr", min=1, max = 6, default=1, description="Set this to 2 or 3 if the baked texture is blurry")
+    my_bool : BoolProperty(
+    name="Enable or Disable", description="Enables debug mode if checked", default = False)
+        
 class KK_Panel(bpy.types.Panel):
     bl_idname = "KK_Panel"
     bl_label = "KK Panel"
@@ -18,6 +32,7 @@ class KK_Panel(bpy.types.Panel):
         row.operator('kkb.separatebody', text = '3 Separate the body') 
         row = layout.row()
         row.operator('kkb.cleanarmature', text = '4 Clean armature')
+        row.prop(context.scene.placeholder, "my_bool", text = "Enable debug mode")
         row = layout.row()
         row.operator('kkb.bonedrivers', text = '5 Add bone drivers')    
         row = layout.row()
@@ -26,5 +41,6 @@ class KK_Panel(bpy.types.Panel):
         row.operator('kkb.importtextures', text = '7 Import textures')
         row = layout.row()
         row.operator('kkb.bakematerials', text = '8 Bake material templates')
+        row.prop(context.scene.placeholder, "inc_dec_int", text = 'Multiplier:')
         row = layout.row()
         row.operator('kkb.applymaterials', text = '9 Apply material templates')
