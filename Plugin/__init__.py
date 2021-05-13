e97c68ef9fb9df64a4343a95aa7b67b3f6b239e2
FlailingFog <65811931+FlailingFog@users.noreply.github.com>
2021-05-13 17:11:36 -0400

    update blender version, remove wiki link

--- Plugin/__init__.py
+++ Plugin/__init__.py
@@ -6,10 +6,9 @@ bl_info = {
     "location" : "View 3D > Tool Shelf > KK Scripts",
     "description" : "Scripts for automating the cleanup process of a Koikatsu export",
     "version": (4, 1, 3), #great now I have to remember to update this number each time
-    "blender" : (2, 91, 2),
+    "blender" : (2, 92, 0),
     "location" : "View3D",
     "category" : "3D View",
-    "wiki_url" : "https://github.com/FlailingFog/KK-Blender-Shader-Pack/wiki",
     "tracker_url" : "https://github.com/FlailingFog/KK-Blender-Shader-Pack/"
 }
 
