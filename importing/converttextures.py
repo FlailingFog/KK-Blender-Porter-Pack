4f4ba9d6e6cd3f47faa03d67b82b220c5a476ed0
FlailingFog <65811931+FlailingFog@users.noreply.github.com>
2024-10-04 16:30:08 -0400

    fix saturation for #603

--- importing/converttextures.py
+++ importing/converttextures.py
@@ -14,10 +14,10 @@ def saturate(image, lut_image):
     vertex_default = '''
     in vec2 a_position;
     in vec2 a_texcoord;
-
+    
     in vec4 color;
     out vec4 col;
-
+    
     void main() {
         gl_Position = vec4(a_position, 0.0, 1.0);
         col = color;
@@ -29,7 +29,7 @@ def saturate(image, lut_image):
     uniform sampler2D tex0;
     uniform sampler2D lut;
     uniform vec2    u_resolution;
-
+    
     in vec4 col;
     out vec4 out_Color;
 
@@ -37,7 +37,7 @@ def saturate(image, lut_image):
         c.rgb = max( 1.055 * pow( c.rgb, vec3(0.416666667,0.416666667,0.416666667) ) - 0.055, 0 );
         return c;
     }
-
+    
     vec3 apply_lut(vec3 color) {
         const vec3 coord_scale = vec3(0.0302734375, 0.96875, 31.0);
         const vec3 coord_offset = vec3( 0.5/1024, 0.5/32, 0.0);
@@ -55,7 +55,7 @@ def saturate(image, lut_image):
         
         vec3 lutColor = mix(lutcol_bot, lutcol_top, coord_frac.z);
         
-        return lutcol_bot;
+        return lutColor;
     }
 
     void main() {
@@ -73,16 +73,19 @@ def saturate(image, lut_image):
 
     # This object gives access to off screen buffers.
     offscreen = gpu.types.GPUOffScreen(width, height)
-
+    
     # Context manager to ensure balanced bind calls, even in the case of an error.
     # Only run if valid
     with offscreen.bind():
+        
         # Clear buffers to preset values
         bgl.glClear(bgl.GL_COLOR_BUFFER_BIT)
+
         # Initialize the shader
         # GPUShader combines multiple GLSL shaders into a program used for drawing. 
         # It must contain a vertex and fragment shaders, with an optional geometry shader.
         shader = gpu.types.GPUShader(vertex_default, current_code)
+        
         # Initialize the shader batch
         # It makes sure that all the vertex attributes necessary for a specific shader are provided.
         batch = batch_for_shader(
@@ -92,10 +95,13 @@ def saturate(image, lut_image):
                 'a_position': ((-1, -1), (1, -1), (1, 1), (-1, 1), (-1, -1)),
             },
         )
+
         # Bind the shader object. Required to be able to change uniforms of this shader.
         shader.bind()
+        
         bgl.glUniform1i(bgl.glGetUniformLocation(shader.program, "tex0"), 0)
         bgl.glUniform1i(bgl.glGetUniformLocation(shader.program, "lut"), 1)
+                    
         try:
             # Make sure image has a bindcode
             if image.bindcode == 0:
@@ -103,6 +109,7 @@ def saturate(image, lut_image):
                     image.gl_load()
                     if image.bindcode != 0:
                         break
+
             # https://docs.blender.org/api/current/bgl.html
             bgl.glActiveTexture(bgl.GL_TEXTURE0)
             bgl.glBindTexture(bgl.GL_TEXTURE_2D, image.bindcode)
@@ -110,18 +117,22 @@ def saturate(image, lut_image):
             bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_WRAP_T, bgl.GL_CLAMP_TO_EDGE)
             bgl.glTexParameterf(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MIN_FILTER, bgl.GL_LINEAR)
             bgl.glTexParameterf(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MAG_FILTER, bgl.GL_LINEAR)
+            
             # Specify the value of a uniform variable for the current program object. 
             # In this case, an image.
             shader.uniform_int("tex0", 0)
         except ValueError:
             pass
+        
         try:
+
             # Make sure image has a bindcode
             if lut_image.bindcode == 0:
                 for i in range(0, 20):
                     lut_image.gl_load()
                     if lut_image.bindcode != 0:
                         break
+
             # https://docs.blender.org/api/current/bgl.html
             bgl.glActiveTexture(bgl.GL_TEXTURE1)
             bgl.glBindTexture(bgl.GL_TEXTURE_2D, lut_image.bindcode)
@@ -129,26 +140,33 @@ def saturate(image, lut_image):
             bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_WRAP_T, bgl.GL_CLAMP_TO_EDGE)
             bgl.glTexParameterf(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MIN_FILTER, bgl.GL_LINEAR)
             bgl.glTexParameterf(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MAG_FILTER, bgl.GL_LINEAR)
+            
             # Specify the value of a uniform variable for the current program object. 
             # In this case, an image.
             shader.uniform_int("lut", 1)
         except ValueError: 
             pass
+
         try:
             shader.uniform_float('u_resolution', (width, height))
         except ValueError: 
             pass
+        
         # Run the drawing program with the parameters assigned to the batch.
         batch.draw(shader)
+
         # The Buffer object is simply a block of memory that is delineated and initialized by the user.
         buffer = bgl.Buffer(bgl.GL_BYTE, width * height * 4)
-        # # Select a color buffer source for pixels.
+        
+        # Select a color buffer source for pixels.
         bgl.glReadBuffer(bgl.GL_BACK)
-        # # Read a block of pixels from the frame buffer.
+        
+        # Read a block of pixels from the frame buffer.
         bgl.glReadPixels(0, 0, width, height, bgl.GL_RGBA, bgl.GL_UNSIGNED_BYTE, buffer)
 
     # Free the offscreen object. The framebuffer, texture and render objects will no longer be accessible.
     offscreen.free()
+
     # Return the final buffer-pixels
     pixels = numpy.array([v / 255 for v in buffer])
     image.pixels = pixels.tolist()
